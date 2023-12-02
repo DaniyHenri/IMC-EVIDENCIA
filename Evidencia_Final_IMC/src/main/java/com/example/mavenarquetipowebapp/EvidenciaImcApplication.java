e9c821d4eea1a1ba5384f87a4575d8ee9dcc2be0
Daniel Rodriguez <al02944110@tecmilenio.mx>
2023-12-01 20:00:36 -0700

    Evidencia

---
+++ Evidencia_Final_IMC/src/main/java/com/example/mavenarquetipowebapp/EvidenciaImcApplication.java
@@ -0,0 +1,13 @@
+package com.example.mavenarquetipowebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvidenciaImcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EvidenciaImcApplication.class, args);
+    }
+
+}
---
+++ Evidencia_Final_IMC/src/test/java/com/example/mavenarquetipowebapp/EvidenciaImcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mavenarquetipowebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvidenciaImcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
